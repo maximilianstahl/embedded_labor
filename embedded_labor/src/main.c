962106c8f9b153afdd7e40a60591244ed21f651e
Stahl Maximilian <Maximilian.Stahl@elektrobit.com>
2024-06-06 09:54:20 +0200

    added atmelstudio files

---
+++ embedded_labor/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * embedded_labor.c
+ *
+ * Created: 06/06/2024 09:52:00
+ * Author : mast274931
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
